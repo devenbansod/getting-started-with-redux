2f5146117191ae69fee773ae46c42ff7f7cefbe2
Robin Murphy <robin.murphy@bbc.co.uk>
2016-03-06 22:38:06 +0000

    Lesson 8
    Redux: React Counter Example

--- counter/app.js
+++ counter/app.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 const counter = (state = 0, action) => {
   switch(action.type) {
     case 'INCREMENT':
@@ -9,38 +13,34 @@ const counter = (state = 0, action) => {
   }
 };
 
-const createStore = (reducer) => {
-  let state;
-  let listeners = [];
-
-  const getState = () => state;
-
-  const dispatch = (action) => {
-    state = reducer(state, action);
-    listeners.forEach(listener => listener());
-  };
-
-  const subscribe = (listener) => {
-    listeners.push(listener);
-    return () => {
-      listeners = listeners.filter(l => l !== listener);
-    };
-  };
-
-  dispatch({});
-
-  return { getState, dispatch, subscribe };
-};
+const Counter = ({
+  value,
+  onIncrement,
+  onDecrement
+}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+);
 
 const store = createStore(counter);
 
 const render = () => {
-  document.body.innerText = store.getState();
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() =>
+        store.dispatch({ type: 'INCREMENT' })
+      }
+      onDecrement={() =>
+        store.dispatch({ type: 'DECREMENT' })
+      }
+      />,
+    document.getElementById('app')
+  );
 };
 
 store.subscribe(render);
 render();
-
-document.addEventListener('click', () => {
-  store.dispatch({ type: 'INCREMENT' });
-});
