19ad944b60fdac160dacff564fe5b8617c283adf
Robin Murphy <robin@robinmurphy.co.uk>
2017-04-30 19:48:17 +0100

    Lesson 2
    Redux: Supplying the Initial State

--- todos/app.js
+++ todos/app.js
@@ -4,8 +4,18 @@ import { Provider } from 'react-redux';
 import TodoApp from './components/TodoApp';
 import configureStore from './configureStore';
 
+const persistedState = {
+  todos: [{
+    id: '0',
+    text: 'Welcome back!',
+    completed: false
+  }]
+};
+
+const store = configureStore(persistedState);
+
 ReactDOM.render(
-  <Provider store={configureStore({})}>
+  <Provider store={store}>
     <TodoApp />
   </Provider>,
   document.getElementById('app')
