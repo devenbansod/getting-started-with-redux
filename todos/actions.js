f65e0022a45d77b68e870c2e1765daf8fa4b6dca
Robin Murphy <robin@robinmurphy.co.uk>
2017-04-30 20:27:26 +0100

    Lesson 6
    Redux: Navigating with React Router <Link>

--- todos/actions.js
+++ todos/actions.js
@@ -1,23 +1,12 @@
 import { v4 } from 'node-uuid';
 
-const addTodo = (text) => ({
+export const addTodo = (text) => ({
   type: 'ADD_TODO',
   id: v4(),
   text,
 });
 
-const setVisibilityFilter = (filter) => ({
-  type: 'SET_VISIBILITY_FILTER',
-  filter
-});
-
-const toggleTodo = (id) => ({
+export const toggleTodo = (id) => ({
   type: 'TOGGLE_TODO',
   id
 });
-
-export {
-  addTodo,
-  setVisibilityFilter,
-  toggleTodo
-};
