0ff91767f0fb4f6a8a39541c1fc60b85f9caa88e
Robin Murphy <robin@robinmurphy.co.uk>
2017-05-01 12:39:18 +0100

    Lesson 18
    Redux: Applying Redux Middleware

--- todos/configureStore.js
+++ todos/configureStore.js
@@ -1,49 +1,23 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducers';
+import promise from 'redux-promise';
+import logger from 'redux-logger';
 
-const logger = store => next => {
-  if (!console.group) {
-    return next;
-  }
-
-  return (action) => {
-    console.group(action.type);
-    console.log('%c prev state', 'color: gray', store.getState());
-    console.log('%c action', 'color: blue', action);
-    const returnValue = next(action);
-    console.log('%c next state', 'color: green', store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
-  };
-};
-
-const promise = () => next => action => {
-  if (typeof action.then === 'function') {
-    return action.then(next);
-  }
-  return next(action);
-};
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-  middlewares.slice().reverse().forEach(middleware =>
-    store.dispatch = middleware(store)(store.dispatch)
-  );
-};
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const configureStore = () => {
-  const store = createStore(
-    reducer,
-    window.devToolsExtension ? window.devToolsExtension() : undefined
-  );
   const middlewares = [promise];
 
   if (process.env.NODE_ENV !== 'production') {
     middlewares.push(logger);
   }
 
-  wrapDispatchWithMiddlewares(store, middlewares);
-
-  return store;
+  return createStore(
+    reducer,
+    composeEnhancers(
+      applyMiddleware(...middlewares)
+    )
+  );
 };
 
 export default configureStore;
