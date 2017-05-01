dd921a5e58d4eedd36d3b82d573559ea42cea35d
Robin Murphy <robin@robinmurphy.co.uk>
2017-05-01 12:13:10 +0100

    Lesson 15
    Redux: Dispatching Actions with the Fetched Data

--- todos/containers/VisibleTodoList.js
+++ todos/containers/VisibleTodoList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { toggleTodo } from '../actions';
+import * as actions from '../actions';
 import { withRouter } from 'react-router';
 import TodoList from '../components/TodoList';
 import { getVisibleTodos } from '../reducers';
@@ -8,21 +8,30 @@ import { fetchTodos } from '../api';
 
 class VisibleTodoList extends Component {
   componentDidMount() {
-    fetchTodos(this.props.filter).then(todos =>
-      console.log(this.props.filter, todos)
-    );
+    this.fetchData();
   }
 
   componentDidUpdate(prevProps) {
     if (prevProps.filter !== this.props.filter) {
-      fetchTodos(this.props.filter).then(todos =>
-        console.log(this.props.filter, todos)
-      );
+      this.fetchData();
     }
   }
 
+  fetchData() {
+    const { filter, receiveTodos } = this.props;
+    fetchTodos(filter).then(todos =>
+      receiveTodos(filter, todos)
+    );
+  }
+
   render() {
-    return <TodoList {...this.props} />;
+    const { toggleTodo, ...rest } = this.props;
+    return (
+      <TodoList
+        {...rest}
+        onTodoClick={toggleTodo}
+      />
+    );
   }
 }
 
@@ -34,11 +43,7 @@ const mapStateToProps = (state, { params }) => {
   };
 };
 
-const mapDispatchToProps = {
-  onTodoClick: toggleTodo,
-};
-
 export default withRouter(connect(
   mapStateToProps,
-  mapDispatchToProps
+  actions
 )(VisibleTodoList));
