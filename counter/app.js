1d698c77ce522f8bc458f2ef22794e4b0bc9c929
Bansod, Deven <debansod@paypal.com>
2018-01-26 14:19:40 +0530

    Lesson 7

--- counter/app.js
+++ counter/app.js
@@ -1,5 +1,5 @@
 import expect from 'expect';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 
 const counter = (state = 0, action) => {
   switch(action.type) {
@@ -12,13 +12,32 @@ const counter = (state = 0, action) => {
   }
 };
 
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    };
+  };
+
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
+};
+
 const store = createStore(counter);
 console.log(store.getState());
-
-/**
-store.dispatch({ type: 'INCREMENT' });
-console.log(store.getState());
-*/
 
 const render = () => {
   document.body.innerText = store.getState();
